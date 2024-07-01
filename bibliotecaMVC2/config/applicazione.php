5f7e84c59a3a62df872e852127ac01123c6cfcb1
20221092 <134252476+20221092@users.noreply.github.com>
2024-07-01 00:02:59 -0600

    Add files via upload

---
+++ bibliotecaMVC2/config/applicazione.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL="http://localhost/bibliotecaMVC2/";
+    const APP_NOME="PRY BIBLIOTECA";
+    const APP_NOME_SESSIONE="BIBLI";
+
+    date_default_timezone_set("America/Monterrey");
